193a8fbb982b26f2c595212f12f5799015a63807
Nikos Kouroupas <nikos.kouroupas@marinetraffic.com>
2018-03-15 12:50:48 +0200

    Reaching out to the Web (Http / Ajax) basics

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Blog from './containers/Blog/Blog';
+
+class App extends Component {
+  render() {
+    return (
+      <Blog />
+    );
+  }
+}
+
+export default App;
