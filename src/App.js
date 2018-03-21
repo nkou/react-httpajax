329492d1c7cce4314fbbc03f799560cea55c149c
Nikos Kouroupas <nikos.kouroupas@marinetraffic.com>
2018-03-21 17:54:36 +0200

    Setting Up the Router Package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <Blog />
+      <BrowserRouter>
+        <Blog />
+      </BrowserRouter>
     );
   }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -34,13 +31,13 @@ class Blog extends Component {
     }
 
     render() {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)} />;
-        });
+        // const posts = this.state.posts.map(post => {
+        //     return <Post
+        //         key={post.id}
+        //         title={post.title}
+        //         author={post.author}
+        //         clicked={() => this.postSelectedHandler(post.id)} />;
+        // });
 
         return (
             <div className="Blog">
@@ -52,15 +49,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
+                {/* <section className="Posts">
                     {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                </section> */}
             </div>
         );
     }
